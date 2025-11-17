840091876f00a2dc4032b429b1012e1a1df313f1
Aaditya Srivastava <aaditya.srivastava.connect@gmail.com>
2025-11-17 19:51:43 +0530

    Initial Commit

---
+++ HeadFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadFlow
+//
+//  Created by Aaditya Srivastava on 17/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadFlow/HeadFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadFlowApp.swift
+//  HeadFlow
+//
+//  Created by Aaditya Srivastava on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HeadFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
