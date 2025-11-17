1bb50d211f66f1705da639319a6b6457e04f8804
sumionochi <aaditya.srivastava.connect@gmail.com>
2025-11-17 20:47:48 +0530

    feat(macOS): add preferences window with stored head scroll settings

---
+++ HeadFlow/PreferencesView.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesView.swift
+//  HeadFlow
+//
+//  Created by Aaditya Srivastava on 17/11/25.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreferencesView()
+}
